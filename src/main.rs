873a323bf51dbe77cb0ae068c8193798baf059ca
James MacMahon <jwm@operand.ca>
2023-06-05 15:10:47 -0400

    rename submod to submod2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("Hello, world! {}", submod::add(7, 8));
+    println!("Hello, world! {}", submod2::add(7, 8));
     println!("Hello, world! {}", binary::operation(7, 8));
 }
